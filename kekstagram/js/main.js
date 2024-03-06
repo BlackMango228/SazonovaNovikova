f7c25b1520739ff376cc3c1c04949483c5e74946
BlackMango228 <159263363+BlackMango228@users.noreply.github.com>
2024-03-06 10:34:02 +0300

    11
    11

--- kekstagram/js/main.js
+++ kekstagram/js/main.js
@@ -1,5 +1,17 @@
-import { getPictures } from './data.js';
+//import { getPictures } from './data.js';
 import { renderPictures } from './picture.js';
 import './form.js';
+import {getData} from './api.js';
 
-renderPictures(getPictures());
+
+//renderPictures(getPictures());
+
+const onLoadSuccess = (data) => {
+  renderPictures(data);
+};
+
+// const onLoadError = (error) => {
+//   // показать ошибку
+// };
+
+getData(onLoadSuccess);
